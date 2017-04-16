03f3a62c95769a57c7242fddd4784f8530ae5548
krishnaparava <krisnareddyparava@gmail.com>
2017-04-16 23:44:34 +0000

    initial commit worksataton

---
+++ workstation/recipes/setup.rb
@@ -0,0 +1,16 @@
+package 'tree' do
+  action :install
+end
+
+package 'ntp' do
+  action :install
+end
+
+file '/etc/motd' do
+  content 'property of krishna'
+  owner 'root'
+  group 'root'
+end
+
+package 'git'
+
